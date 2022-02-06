d996a741f40baf9ae2867dc5bc0025fcfffa93bc
UsernameForTheWin <97945494+UsernameForTheWin@users.noreply.github.com>
2022-02-06 13:07:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Bot Configuration
 # Make sure you paste your 'server_id' as an integer and not a string
-bot_token = "TOKEN"
+bot_token = "OTM5Nzg2NTIxMjExNDQxMTYy.Yf96Zw.r0AnuiJR6Qdp2k3XNCS2BPMfL_w"
 server_id = ID
 
 category_prefix = "SierraOne"
-channel_prefix = "sierra-hotel-"
+channel_prefix = "+++"
 
 # Mega Configuration (OPTIONAL)
 # Leave this as it is if you'd rather have the large files to be 
